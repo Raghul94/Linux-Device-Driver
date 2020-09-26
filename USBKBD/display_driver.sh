97eb89be53ecb899b46186102bd05ee7fe663cb2
S Raghul <sraghul.us@gmail.com>
2020-09-26 13:38:18 -0700

    Adding Files

---
+++ USBKBD/display_driver.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+ls -l /sys/bus/usb/devices
+
+ls -l /sys/bus/usb/drivers/usbhid/
+
+ls -l /sys/bus/usb/drivers/usbkbd/
---
+++ USBKBD/unbind_bind.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+echo -n "1-2:1.0" >  /sys/bus/usb/drivers/usbhid/unbind
+
+echo -n "1-2:1.0" >  /sys/bus/usb/drivers/usbkbd/bind
